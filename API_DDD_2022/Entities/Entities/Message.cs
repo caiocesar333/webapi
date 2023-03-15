ed8df16d22bd8ebbcbbb4f7691505311845acfd1
Caio <caiocesar333@outlook.com>
2023-03-15 11:33:27 -0300

    Feat: First Commit and initial setup

---
+++ API_DDD_2022/Entities/Entities/Message.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    [Table("TB_MESSAGE")]
+    public class Message:Notifies
+    {
+        [Column("MSN_ID")]
+        public int Id { get; set; }
+
+        [Column("MSN_TITLE")]
+        [MaxLength(255)]
+        public string Title { get; set; }
+
+        [Column("MSN_ACTIVE")]
+        public bool Active { get; set; }
+
+        [Column("MSN_DATE_REGISTER")]
+        public DateTime DateRegister { get; set; }
+
+        [Column("MSN_DATE_UPDATE")]
+        public DateTime DateUpdate { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        [Column(Order = 1)]
+        public string UserId { get; set; }
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+    }
+}
---
+++ API_DDD_2022/Entities/Entities/Notifies.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    public class Notifies
+    {
+
+        public Notifies() 
+        {   
+            
+            Notitycoes = new List<Notifies>();
+
+        }   
+
+        [NotMapped]
+        public string NameProperty { get; set; }
+
+        [NotMapped]
+        public string Message { get; set; }
+
+        [NotMapped]
+        public List<Notifies> Notitycoes { get; set; }
+
+
+        public bool ValidatePropertyString(string value, string NameProperty)
+        {
+            if (string.IsNullOrWhiteSpace(value) || string.IsNullOrWhiteSpace(NameProperty)) 
+            {
+                Notitycoes.Add(new Notifies
+                {
+                    Message = "Required field",
+                    NameProperty = NameProperty
+                });
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool ValidatePropertyInt(int value, string NameProperty)
+        {
+            if ( value < 1 || string.IsNullOrWhiteSpace(NameProperty))
+            {
+                Notitycoes.Add(new Notifies
+                {
+                    Message = "Required field",
+                    NameProperty = NameProperty
+                });
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
