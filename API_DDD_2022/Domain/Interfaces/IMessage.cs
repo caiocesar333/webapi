3f5f1637d1a781ce9de402897f15afd311fd7a07
Caio <caiocesar333@outlook.com>
2023-03-16 16:30:52 -0300

    Fix: refactoring infra and domain

---
+++ API_DDD_2022/Domain/Interfaces/IMessage.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Interfaces.Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Interfaces.Generics;
+using Entities.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IMessage : IGeneric<Message>
+    {
+
+
+    }
+}
